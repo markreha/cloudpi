2cd5438770ddc6dacb70ebdc8789995f4ef41253
Mark Reha <markreha@mac.com>
2017-07-10 15:45:53 +0000

    Added Secondary Endpoint support

--- app/hat/Config.py
+++ app/hat/Config.py
@@ -7,7 +7,7 @@ sampleTime = 1800
 # Device ID for this PI
 deviceID = 0
 
-# Different environment configurations
+# Different Primary environment configurations
 env_dev1 = {'webApi': 'http://marks-macbookair.local:8080/cloudservices/rest/weather/save',
          	'username': 'CloudWorkshop',
          	'password': 'dGVzdHRlc3Q='}
@@ -20,3 +20,10 @@ env_qa = {'webApi': 'http://cloudservices-workshop.1d35.starter-us-east-1.opensh
 env_prod = {'webApi': 'http://cloudservices-workshop.1d35.starter-us-east-1.openshiftapps.com/cloudservices/rest/weather/save',
          	'username': 'CloudWorkshop',
          	'password': 'dGVzdHRlc3Q='}
+
+# Optional Secondary endpoints to post data
+process_sec_endpoints = False
+sec_endpoints = [
+			['Test 1', 'http://somewhere.com/cloudservices/rest/weather/save', 'CloudWorkshop', 'dGVzdHRlc3Q='],
+			['Test 2', 'http://somewhere.com/cloudservices/rest/weather/save', 'CloudWorkshop', 'dGVzdHRlc3Q=']
+		]
